53031f5d71493c9e98b19de9b5b88bbc2a0cd730
Chris Ineson <chrisineson@lloydsbanking.com>
2021-04-27 19:43:40 +0100

    v1 release

--- dist/index.js
+++ dist/index.js
@@ -13,8 +13,8 @@ const wait = __nccwpck_require__(258);
 async function run() {
   try {
     const ms = core.getInput('milliseconds');
+    core.info(`Action started`);
     core.info(`Waiting ${ms} milliseconds ...`);
-
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
